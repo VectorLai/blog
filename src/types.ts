e0d3d58bfbcbfbd333c254692ade74acf4a669a7
Vector Lai <vectorlai@@proton.me>
2023-02-26 17:24:35 +0800

    init

---
+++ src/types.ts
@@ -0,0 +1,33 @@
+export type SocialObjects = {
+  name: SocialMedia;
+  href: string;
+  active: boolean;
+  linkTitle: string;
+}[];
+
+export type SocialIcons = {
+  // eslint-disable-next-line no-unused-vars
+  [social in SocialMedia]: string;
+};
+
+export type SocialMedia =
+  | "Github"
+  | "Facebook"
+  | "Instagram"
+  | "LinkedIn"
+  | "Mail"
+  | "Twitter"
+  | "Twitch"
+  | "YouTube"
+  | "WhatsApp"
+  | "Snapchat"
+  | "Pinterest"
+  | "TikTok"
+  | "CodePen"
+  | "Discord"
+  | "GitLab"
+  | "Reddit"
+  | "Skype"
+  | "Steam"
+  | "Telegram"
+  | "Mastodon";
